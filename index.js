23ebcf4250172cf5ba52bb45b5dfa2434474439f
Berkeley Martinez <Berkeley@robotie.com>
2015-03-05 17:11:51 -0800

    Abstracted out isMobile logic

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
 'use strict';
-
 var React = require('react');
+var isMobile = require('./utils/isMobile')();
 var CodeMirror;
 
-// adapted from:
-// https://github.com/facebook/react/blob/master/docs/_js/live_editor.js#L16
-
-// also used as an example:
-// https://github.com/facebook/react/blob/master/src/browser/ui/dom/components/ReactDOMInput.js
-
-var IS_MOBILE = typeof navigator === 'undefined' || (
-  navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-);
-
-if (!IS_MOBILE) {
+if (!isMobile) {
   CodeMirror = require('codemirror');
 }
 
 var CodeMirrorEditor = React.createClass({
+
   getInitialState: function() {
     return { isControlled: this.props.value != null };
   },
 
   propTypes: {
-    value: React.PropTypes.string,
-    defaultValue: React.PropTypes.string,
-    style: React.PropTypes.object,
     className: React.PropTypes.string,
-    onChange: React.PropTypes.func
+    config: React.PropTypes.object,
+    defaultValue: React.PropTypes.string,
+    onChange: React.PropTypes.func,
+    style: React.PropTypes.object,
+    value: React.PropTypes.string,
   },
 
   componentDidMount: function() {
-    var isTextArea = this.props.forceTextArea || IS_MOBILE;
+    var isTextArea = this.props.forceTextArea || isMobile;
     if (!isTextArea) {
       this._editor =
         CodeMirror.fromTextArea(this.refs.editor.getDOMNode(), this.props);
@@ -92,3 +78,10 @@ var CodeMirrorEditor = React.createClass({
 });
 
 module.exports = CodeMirrorEditor;
+
+// adapted from:
+// https://github.com/facebook/react/blob/master/docs/_js/live_editor.js#L16
+
+// also used as an example:
+// https://github.com/facebook/react/blob/master/src/browser/ui/dom/components/ReactDOMInput.js
+
